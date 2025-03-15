b64fff376d0278bc4f41577c8b8be1ed37dcad5f
gopikrishnan-M <gopikris0021@gmail.com>
2025-03-15 11:38:24 +0530

    program modified

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,7 +6,15 @@ public class Fibonacci {
         // if possible then write a formula for that this formula is known as recurrence relation
         // now try to figure out the recursive tree
         //always try to see three variable passing parameter,body, return variable.
-        System.out.println(fibo(5));
+        System.out.println(fibo(5));//this has exponential time complexity so for some large numbers like 50 it wont give answers
+        System.out.println(fiboFormulae(13));;//this is a derived formula using time complexity and is the best way
+    }
+
+    private static int fiboFormulae(int n) {
+        return (int)((1/Math.sqrt(5))*(Math.pow((1 + Math.sqrt(5)) / 2, n) - Math.pow((1 - Math.sqrt(5)) / 2, n)));
+
+       /* double v=Math.pow((1+Math.sqrt(5))/2,n)/Math.sqrt(5);//another formula which ignores the less dominating term
+        return (int)v;*/
     }
 
     private static int fibo(int n) {
